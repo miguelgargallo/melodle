25dfc84c04b9a6d748aee8187fe28c0d233aa80d
Miguel Gargallo <mgargallo16@gmail.com>
2022-07-01 19:54:03 +0200

    2.2.3 Addons

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,32 +1,32 @@
 export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
-export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
-export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
+export const WIN_MESSAGES = ['Buen trabajo!', 'Ole', 'Bien Culito!']
+export const GAME_COPIED_MESSAGE = 'Juego copiado del portapeles'
+export const NOT_ENOUGH_LETTERS_MESSAGE = '¿Que fumas?'
+export const WORD_NOT_FOUND_MESSAGE = 'Ni de coña!!'
 export const HARD_MODE_ALERT_MESSAGE =
-  'Hard Mode can only be enabled at the start!'
+  'Modo Dificil? Tolai ve al menu y activalo ahi!'
 export const HARD_MODE_DESCRIPTION =
-  'Any revealed hints must be used in subsequent guesses'
-export const HIGH_CONTRAST_MODE_DESCRIPTION = 'For improved color vision'
+  'Las pistas han de ser consecutivas, o pa tu casa'
+export const HIGH_CONTRAST_MODE_DESCRIPTION = 'Pa los daltonicos o amantes del minimalismo'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `The word was ${solution}`
+  `A ver, la palabra es ${solution}`
 export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
-  `Must use ${guess} in position ${position}`
+  `Bobi, debe usar ${guess} in position ${position}`
 export const NOT_CONTAINED_MESSAGE = (letter: string) =>
-  `Guess must contain ${letter}`
-export const ENTER_TEXT = 'Enter'
-export const DELETE_TEXT = 'Delete'
-export const STATISTICS_TITLE = 'Statistics'
-export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
-export const NEW_WORD_TEXT = 'New word in'
-export const SHARE_TEXT = 'Share'
-export const MIGRATE_BUTTON_TEXT = 'Transfer'
+  `Atente coño, debe contener ${letter}`
+export const ENTER_TEXT = 'Intro'
+export const DELETE_TEXT = 'Borra'
+export const STATISTICS_TITLE = 'Stats'
+export const GUESS_DISTRIBUTION_TEXT = 'Distribucion posible'
+export const NEW_WORD_TEXT = 'Relaja la raja culito'
+export const SHARE_TEXT = 'Compartelo almenos'
+export const MIGRATE_BUTTON_TEXT = 'Transfierelo a otro melodle pc'
 export const MIGRATE_DESCRIPTION_TEXT =
-  'Click here to transfer your statistics to a new device.'
-export const TOTAL_TRIES_TEXT = 'Total tries'
-export const SUCCESS_RATE_TEXT = 'Success rate'
-export const CURRENT_STREAK_TEXT = 'Current streak'
-export const BEST_STREAK_TEXT = 'Best streak'
+  'Click aqui pa transferir tus stats de pro de mierda a otro melodle pc.'
+export const TOTAL_TRIES_TEXT = 'Intentos que llevas'
+export const SUCCESS_RATE_TEXT = 'Victorias? Aqui'
+export const CURRENT_STREAK_TEXT = 'Streak actual'
+export const BEST_STREAK_TEXT = 'Mejor streak'
 export const DISCOURAGE_INAPP_BROWSER_TEXT =
-  "You are using an embedded browser and may experience problems sharing or saving your results. We encourage you rather to use your device's default browser."
+  "Bro mejora tu buscador que puede que tengas problemas, un saludo, Miguel Gargallo."
