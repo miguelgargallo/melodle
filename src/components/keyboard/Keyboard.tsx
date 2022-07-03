32b6fd6bd3a7acd5dc2f325411c153429f61238a
Miguel Gargallo <mgargallo16@gmail.com>
2022-07-03 15:22:54 +0200

    2.1.1 Fumada fix

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -96,7 +96,7 @@ export const Keyboard = ({
         ))}
       </div>
       <div className="flex justify-center">
-        {['Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Ç', ''<img jsname="fpF2df" class="smm4bb" src="https://www.google.com/logos/fnbx/pride/icon_dm.gif">''].map((key) => (
+        {['Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Ç', '<img jsname="fpF2df" class="smm4bb" src="https://www.google.com/logos/fnbx/pride/icon_dm.gif">'].map((key) => (
           <Key
             value={key}
             key={key}
