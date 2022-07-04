17c54502cf55c6ec09cea572b8b7c1fa35b402a2
Miguel Gargallo <mgargallo16@gmail.com>
2022-07-04 08:26:43 +0200

    1.0.2 fix

--- src/constants/validGuesses.ts
+++ src/constants/validGuesses.ts
@@ -1,6 +1,79 @@
 // Miguel Gargallo 2022 (c) All Rights Reserved
   // Melo Community added by the community and Miguel Gargallo
   export const VALID_GUESSES = [
+  '24🥖🥖🥖',
+  '🍑culo',
+  'culo🍑',
+  '🏳️‍🌈free',
+  'free🏳️‍🌈',
+  '🏳️‍⚧️free',
+  'free🏳️‍⚧️',
+  '💦sexo',
+  'sexo💦',
+  '🍆sexo',
+  'sexo🍆',
+  '🤡joke',
+  'joke🤡',
+  // Letra A
+  'aaaaa',
+  // Letra B
+  'barna',
+  'boobs',
+  'booty',
+  // Letra C
+  'coños',
+  'culos',
+  'chloe',
+  'croft',
+  // Letra D
+  'dicks',
+  // Letra E
+  'ellie',
+  // Letra F
+  // Letra G
+  'gatos',
+  'grito',
+  // Letra H
+  // Letra I
+  // Letra J
+  // Letra K
+  // Letra L
+  'lesbi',
+  'lucas',
+  'lgbtq',
+  // Letra M
+  'music',
+  'mulan',
+  // Letra N
+  // Letra Ñ
+  // Letra O
+  'ortos',
+  // Letra P
+  'pedos',
+  'pedro',
+  'penes',
+  'penis',
+  'pride',
+  'prime',
+  'pussy',
+  'porro',
+  // Letra Q
+  'queer',
+  // Letra R
+  'riley',
+  // Letra S
+  'sonia',
+  'sinpa',
+  'simba',
+  // Letra T
+  'tetis',
+  // Letra U
+  // Letra V
+  // Letra W
+  // Letra X
+  // Letra Y
+  // Letra Z
+  'zorra',
   // French added by Miguel Gargallo
   'abats',
   'abdos',
